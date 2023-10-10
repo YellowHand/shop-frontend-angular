8ad1fa202566a5bf24fb9847cfd5332892b67962
pawel <ypawelborkowski@gmail.com>
2023-10-10 13:50:28 +0200

    fullpage layout

--- src/app/layouts/fullpage/fullpage.module.ts
+++ src/app/layouts/fullpage/fullpage.module.ts
@@ -1,19 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DefaultComponent } from './default.component';
-import { HomeComponent } from 'src/app/modules/home/home.component';
-import { ProductComponent } from 'src/app/modules/product/product.component';
+import { FullpageComponent } from './fullpage.component';
 import { RouterModule } from '@angular/router';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { LoginComponent } from 'src/app/modules/login/login.component';
 
 
 
 @NgModule({
   declarations: [
-    DefaultComponent,
-    HomeComponent,
-    ProductComponent,
+    FullpageComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
@@ -22,4 +20,4 @@ import { SharedModule } from 'src/app/shared/shared.module';
     SharedModule
   ]
 })
-export class DefaultModule { }
+export class FullpageModule { }
